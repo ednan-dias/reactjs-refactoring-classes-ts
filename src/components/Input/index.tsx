8c015502cd8cf8009d84e96490fb802bfbac9477
Ednan Dias <ednan.741@gmail.com>
2021-04-05 20:47:51 -0300

    finalizando projeto em typescript

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,15 +1,16 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
+import { useEffect, useRef, useState, useCallback } from "react";
 
-import { useField } from '@unform/core';
+import { useField } from "@unform/core";
+import { Container } from "./styles";
+import { IconType } from "react-icons";
 
-import { Container } from './styles';
+interface InputProps {
+  name: string;
+  icon?: IconType;
+  placeholder: string
+}
 
-const Input = ({ name, icon: Icon, ...rest }) => {
+export function Input({ name, icon: Icon, ...rest }: InputProps) {
   const inputRef = useRef(null);
 
   const [isFocused, setIsFocused] = useState(false);
@@ -24,14 +25,14 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
 
-    setIsFilled(!!inputRef.current?.value);
+    setIsFilled(!!inputRef.current);
   }, []);
 
   useEffect(() => {
     registerField({
       name: fieldName,
       ref: inputRef.current,
-      path: 'value',
+      path: "value",
     });
   }, [fieldName, registerField]);
 
@@ -48,6 +49,6 @@ const Input = ({ name, icon: Icon, ...rest }) => {
       />
     </Container>
   );
-};
+}
 
 export default Input;
